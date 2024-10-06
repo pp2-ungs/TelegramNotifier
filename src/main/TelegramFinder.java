4209acdce4f2341ac3b3ebbc7302ae01cc485faa
Gonza López <gonzagonzalopez20@gmail.com>
2024-10-06 18:45:21 -0300

    Add TelegramFinder

---
+++ src/main/TelegramFinder.java
@@ -0,0 +1,21 @@
+package main;
+
+import com.google.gson.Gson;
+import core.Settings;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TelegramFinder {
+    
+    public static Map<String, Number> getTelegramIDMap() throws IOException {
+        Gson gson = new Gson();
+        
+        try (BufferedReader reader = new BufferedReader(new FileReader(Settings.RESOURCES + "Telegram.json"))) {
+            return gson.fromJson(reader, HashMap.class);
+        }
+    }
+    
+}
--- src/main/TelegramNotificator.java
+++ src/main/TelegramNotificator.java
@@ -1,31 +1,38 @@
 package main;
 
+import java.io.IOException;
 import observer.Observer;
 import java.util.Map;
 
 public class TelegramNotificator implements Observer {
 
+    private Map<String, Number> membersIDs;
+
+    public TelegramNotificator() {
+        try {
+            membersIDs = TelegramFinder.getTelegramIDMap();
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+    }
+
     @Override
     public void update(Object event) {
 
         //\begin{FIXME}
-        var memberTelegramId = Integer.parseInt((String) ((Map) event).get("TelegramId"));
-        var taskDescription = (String) ((Map) event).get("Task");
-        var memberName = (String) ((Map) event).get("Name");
+        String taskDescription = (String) ((Map) event).get("Task");
+        String memberName = (String) ((Map) event).get("Name");
         //\end{FIXME}
 
-        StringBuilder msg = new StringBuilder();
-        msg.append("Hola ");
-        msg.append(memberName);
-        msg.append("!\nTenés una nueva tarea!\nSe te asignó la tarea: ");
-        msg.append(taskDescription);
-        msg.append("\nSaludos!");
+        Long memberTelegramId = membersIDs.get(memberName).longValue();
+
+        String msg = "Hola " + memberName + "!\nTenés una nueva tarea!\nSe te asignó la tarea: " + taskDescription + "\nSaludos!";
 
         new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
-                    new Telegram().sendMessageToUser(memberTelegramId, msg.toString());
+                    new Telegram().sendMessageToUser(memberTelegramId, msg);
                 } catch (Exception ex) {
                     System.out.println("?telegram not working");
                 }
@@ -40,6 +47,5 @@ public class TelegramNotificator implements Observer {
     public String getName() {
         return "Telegram notification";
     }
-    */
-
+     */
 }
