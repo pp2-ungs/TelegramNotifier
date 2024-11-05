1f2043011080d613060f4b4d6672729b488107b6
Lucifer! <6011234+rondelli@users.noreply.github.com>
2024-11-05 13:52:33 -0300

    Minor fixes

--- src/telegram/TelegramNotifier.java
+++ src/telegram/TelegramNotifier.java
@@ -6,11 +6,11 @@ import java.util.Map;
 
 public class TelegramNotifier implements Observer {
 
-    private Map<String, Number> membersIDs;
+    private Map<String, Number> personIDs;
 
     public TelegramNotifier() {
         try {
-            membersIDs = TelegramFinder.getTelegramIDMap();
+            personIDs = TelegramFinder.getTelegramIDMap();
         } catch (IOException ex) {
             System.out.println(ex);
         }
@@ -19,17 +19,17 @@ public class TelegramNotifier implements Observer {
     @Override
     public void update(Object event) {
         String taskDescription = (String) ((Map) event).get("Task");
-        String memberName = (String) ((Map) event).get("Name");
+        String personName = (String) ((Map) event).get("Name");
 
-        Long memberTelegramId = membersIDs.get(memberName).longValue();
+        Long telegramID = personIDs.get(personName).longValue();
 
-        String msg = "Hola " + memberName + "!\nTenés una nueva tarea!\nSe te asignó la tarea: " + taskDescription + "\nSaludos!";
+        String msg = "Hola " + personName + "!\nTenés una nueva tarea!\nSe te asignó la tarea: " + taskDescription + "\nSaludos!";
 
         new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
-                    new Telegram().sendMessageToUser(memberTelegramId, msg);
+                    new Telegram().sendMessageToUser(telegramID, msg);
                 } catch (Exception ex) {
                     System.out.println("?telegram not working");
                 }
